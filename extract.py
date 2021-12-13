f1a57b08ee3b93a1d724ee6458c85b1d1f622207
liao_zhenghan <liaozhenghan@outlook.com>
2021-12-13 21:08:26 +0800

    extract: renamed from stitch, upgraded

--- extract.py
+++ extract.py
@@ -1,18 +1,16 @@
 #!/usr/bin/env python
-""" stitch: stitching clusters
+""" extract: stitch clusters and extract connected components
 """
 
 import numpy as np
 import pandas as pd
 import igraph
-import bidict
-from synseg.utils import coord_to_mask, reverse_coord
 
 __all__ = [
     # stitching
     "build_graph",
     # extraction
-    "graph_components", "subgraph_to_image",
+    "graph_components", "labels_to_image"
 ]
 
 #=========================
@@ -20,62 +18,67 @@ __all__ = [
 #=========================
 
 def build_graph_vertices(labels3d):
-    """ build vertices from each cluster(label)
+    """ build vertices from cluster labels
     :param label3d: array[nz,ny,nx], clusters are indexed from 1
-    :return: vertices, vs_attrs
-        vertices: array of vertices, start from 0 (label-1)
+    :return: vs_labels, vs_attrs
+        vs_labels: labels of clusters
         vs_attrs: {weight}
     """
     # use pandas to sort clusters by weight
     df3d = (pd.Series(labels3d[labels3d > 0])
             .value_counts().to_frame("weight")
-            .reset_index()
-            )
+        )
 
-    # extract index and attributes
-    vertices = (df3d["index"]-1).to_list()
+    # generate index and attributes
+    vs_label = df3d.index.to_list()
     vs_attrs = dict(
         weight=df3d["weight"].to_list(),
     )
-    return vertices, vs_attrs
+    return vs_label, vs_attrs
 
-
-def build_graph_edges_adjacent(iz, labels3d):
+def build_graph_edges_adjacent(iz, labels3d, dict_label_vid):
     """ stitch vertices in adjacent slices iz and iz-1
     :param iz: z index, should >=1
     :param label3d: array[nz,ny,nx], clusters are indexed from 1
+    :param dict_label_vid: {label: vid}
     :return: edges, es_attrs
-        edges: array of edges, (v1, v2)
+        edges: array of edges, (vid1, vid2)
         es_attrs: {weight}
     """
-    # calculate overlap between curr and prev, get edges
+    # setups
     edges = []
     es_attrs = {"weight": []}
-    # loop over current clusters
-    yx_shape = labels3d[0].shape
-    labels_prev = labels3d[iz-1]
-    labels_curr = labels3d[iz]
 
-    for i in np.unique(labels_curr[labels_curr>0]):
-        # calculate overlap
-        # count number of each elements using pandas
-        # value=j+1 indicates overlap with prev cluster-j
-        mask_curr_i = np.zeros(yx_shape, dtype=int)
-        mask_curr_i[labels_curr == i] = 1
-        overlap = mask_curr_i * labels_prev
+    yx_shape = labels3d[0].shape
+    labels2d_prev = labels3d[iz-1]
+    labels2d_curr = labels3d[iz]
+
+    # unique labels of iz, note to exclude label=0
+    labels_uniq_curr = np.unique(labels2d_curr[labels2d_curr>0])
+
+    # loop over current clusters
+    for i in labels_uniq_curr:
+        # calculate overlap using image-based method
+        # value=j+1 indicates cluster-i overlaps with prev cluster-j
+        # count each cluster using pandas
+        mask_curr_i = np.zeros(yx_shape, dtype=np.int_)
+        mask_curr_i[labels2d_curr == i] = 1
+        overlap = mask_curr_i * labels2d_prev
         counts = pd.Series(overlap[overlap > 0]).value_counts()
 
         # generate edges
-        for j, weight in zip(counts.index, counts.values):
-            edges.append((i-1, j-1))
-            es_attrs["weight"].append(weight)
+        # note to convert label to vertex id
+        vid_i = dict_label_vid[i]
+        edges_curr = [(vid_i, dict_label_vid[j]) for j in counts.index]
+        edges.extend(edges_curr)
+        es_attrs["weight"].extend(list(counts.values))
 
     return edges, es_attrs
 
-
-def build_graph_edges(labels3d):
+def build_graph_edges(labels3d, dict_label_vid):
     """ stitch vertices through all slices in the stack
-        :param label3d: array[nz,ny,nx], clusters are indexed from 1
+    :param label3d: array[nz,ny,nx], clusters are indexed from 1
+    :param dict_label_vid: {label: vid}
     :return: edges, es_attrs
         edges: array of edges, (v1, v2)
         es_attrs: {weight}
@@ -86,7 +89,7 @@ def build_graph_edges(labels3d):
     # stitch adjacent slices one by one
     for iz in range(1, nz):
         edges_i, es_attrs_i = build_graph_edges_adjacent(
-            iz, labels3d
+            iz, labels3d, dict_label_vid
         )
         edges.extend(edges_i)
         for key, value in es_attrs_i.items():
@@ -94,28 +97,31 @@ def build_graph_edges(labels3d):
             es_attrs.setdefault(key, []).extend(value)
     return edges, es_attrs
 
-
 def build_graph(labels3d):
     """ build graph from clusters
     :param label3d: array[nz,ny,nx], clusters are indexed from 1
     :return: g=igraph.Graph()
-        vertices: 0 to n, attrs=[weight, c3d, iz, c2d]
-        edges: attrs=[weight]
+        vertices: 0 to n, attrs={weight,label}
+        edges: attrs={weight}
     """
-    # get vertices and weights
-    vertices, vs_attrs = build_graph_vertices(labels3d)
-
-    # get edges and weights
-    edges, es_attrs = build_graph_edges(labels3d)
-
-    # build graph
+    # build graph vertices
+    # vs["name"]=vs_labels, vertexID=range(0, n)
     g = igraph.Graph()
-    g.add_vertices(vertices)
-    g.add_edges(edges)
+    vs_label, vs_attrs = build_graph_vertices(labels3d)
+    g.add_vertices(vs_label)
     for key, value in vs_attrs.items():
         g.vs[key] = value
+
+    # build graph edges
+    dict_label_vid = dict(
+        g.get_vertex_dataframe()
+        .reset_index()[["name", "vertex ID"]].values
+    )
+    edges, es_attrs = build_graph_edges(labels3d, dict_label_vid)
+    g.add_edges(edges)
     for key, value in es_attrs.items():
         g.es[key] = value
+
     return g
 
 
@@ -124,10 +130,10 @@ def build_graph(labels3d):
 #=========================
 
 def graph_components(g):
-    """ 
+    """ extract connected components for graph
     :param: graph from build_graph()
     :return: df_comps, g_comps
-        df_comps: columns=[index,nv,ne,v_weight,e_weight,iz_min,iz_max,iz_span]
+        df_comps: columns=[index,nv,ne,v_weight,e_weight]
         g_comps: [vs of comp0, vs of comp1, ...]
     """
     # fields to collect
@@ -137,63 +143,101 @@ def graph_components(g):
     ]
 
     # collect info for each component
-    g_comps = list(g.components())
-    comp_arr = []
-    for i, vi in enumerate(g_comps):
+    label_comps = []
+    df_comps_row = []
+    for i, vi in enumerate(g.components()):
         gi = g.subgraph(vi)
+        # info of the component
         v_weight = np.sum(gi.vs["weight"])
         e_weight = np.sum(gi.es["weight"])
         comp_i = (
             i, gi.vcount(), gi.ecount(),
             v_weight, e_weight
         )
-        comp_arr.append(comp_i)
+        df_comps_row.append(comp_i)
+        # labels of the vertices
+        label_comps.append(gi.vs["name"])
 
     # make a DataFrame
     df_comps = pd.DataFrame(
-        data=comp_arr,
+        data=df_comps_row,
         columns=columns
     )
     df_comps = (df_comps
-                .sort_values("v_weight", ascending=False)
-                .reset_index(drop=True)
-                )
-    return df_comps, g_comps
+        .sort_values("v_weight", ascending=False)
+        .reset_index(drop=True)
+    )
 
+    return df_comps, label_comps
 
-def subgraph_to_image(
-    g, vertices,
-    xyo_stack, labels_stack, zyx_shape
-):
-    """ convert subgraph of clusters to image
-    :param g: graph of clusters
-    :param vertices: vertex IDs in graph, e.g. g_comps[i]
-    :param xyo_stack, labels_stack: results of cluster3d()
-    :param zyx_shape: shape of image, [nz,ny,nx]
-    :return: image I
+def labels_to_image(labels3d, labels_selected):
+    """ convert selected labels to image (1 at labels, 0 otherwise)
+    :param label3d: array[nz,ny,nx], clusters are indexed from 1
+    :param labels_selected: array of labels to select from
+    :return: I
     """
-    # generate xyz for each cluster
-    gi = g.subgraph(vertices)
-    xyz = None
-    for i in range(gi.vcount()):
-        # get iz, c2d
-        iz = gi.vs["iz"][i]
-        c2d = gi.vs["c2d"][i]
-        # select points from slice iz
-        mask_c2d = np.asarray(labels_stack[iz]) == c2d
-        xy_i = xyo_stack[iz][mask_c2d][:, :2]
-        # combine xy with z
-        xyz_i = np.concatenate((xy_i, np.ones((len(xy_i), 1))*iz), axis=1)
-        # concaternate xyz's
-        if xyz is None:
-            xyz = xyz_i
-        else:
-            xyz = np.concatenate((xyz, xyz_i), axis=0)
-
-    # convert xyz to image
-    zyx = reverse_coord(xyz)
-    I = coord_to_mask(zyx, zyx_shape)
+    mask = np.isin(labels3d, labels_selected)
+    I = np.zeros(labels3d.shape, dtype=np.int_)
+    I[mask] = 1
     return I
+
+#=========================
+# extract largest
+#=========================
+
+def extract_connected(labels3d, n_largest=2):
+    """ extract connected clusters
+    :param label3d: array[nz,ny,nx], clusters are indexed from 1
+    :param n_largest: return n_largest connected components
+    :return: L, df_comps
+        L: image of components, labeled from 1
+        df_comps: columns=[index,nv,ne,v_weight,e_weight]
+    """
+    # find connected
+    g = build_graph(labels3d)
+    df_comps, label_comps = graph_components(g)
+
+    # convert to image
+    L = np.zeros(labels3d.shape, dtype=np.int_)
+    for i in range(n_largest):
+        idx = df_comps["index"][i]
+        L_i = labels_to_image(labels3d, label_comps[idx])
+        L += (i+1)*L_i
+    return L, df_comps
+
+# def subgraph_to_image(
+#         g, vertices,
+#         xyo_stack, labels_stack, zyx_shape
+#     ):
+#     """ convert subgraph of clusters to image
+#     :param g: graph of clusters
+#     :param vertices: vertex IDs in graph, e.g. g_comps[i]
+#     :param xyo_stack, labels_stack: results of cluster3d()
+#     :param zyx_shape: shape of image, [nz,ny,nx]
+#     :return: image I
+#     """
+#     # generate xyz for each cluster
+#     gi = g.subgraph(vertices)
+#     xyz = None
+#     for i in range(gi.vcount()):
+#         # get iz, c2d
+#         iz = gi.vs["iz"][i]
+#         c2d = gi.vs["c2d"][i]
+#         # select points from slice iz
+#         mask_c2d = np.asarray(labels_stack[iz]) == c2d
+#         xy_i = xyo_stack[iz][mask_c2d][:, :2]
+#         # combine xy with z
+#         xyz_i = np.concatenate((xy_i, np.ones((len(xy_i), 1))*iz), axis=1)
+#         # concaternate xyz's
+#         if xyz is None:
+#             xyz = xyz_i
+#         else:
+#             xyz = np.concatenate((xyz, xyz_i), axis=0)
+
+    # # convert xyz to image
+    # zyx = reverse_coord(xyz)
+    # I = coord_to_mask(zyx, zyx_shape)
+    # return I
 
 
 # #=========================
