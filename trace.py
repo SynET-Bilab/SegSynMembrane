c2114f67dcec79305fa40f7cb877f7572131e3fa
liao_zhenghan <liaozhenghan@outlook.com>
2021-12-23 22:32:14 +0800

    trace.py: added MemGraph

--- trace.py
+++ trace.py
@@ -3,13 +3,19 @@
 """
 
 import numpy as np
+import scipy as sp
 import pandas as pd
+import networkx as nx
+from synseg.utils import draw_line
 
 __all__ = [
     # trace within segment
-    "trace_within_seg", "segs_to_frame",
+    "trace_within_seg", "trace_within_labels",
     # trace across segments
-    "across_next_seg"
+    "across_next_seg",
+    # graph methods
+    "pathL_to_LE", "pathLE_to_yx", "pathLEs_distance",
+    "MemGraph"
 ]
 
 
@@ -151,22 +157,26 @@ def trace_within_seg(nms, O):
     success = success_plus and success_minus
     return yx_trace, d_trace, success
 
-def segs_to_frame(L, O, n_avg):
-    """ convert segments ends info to dataframe
+def trace_within_labels(L, O, n_avg):
+    """ trace segments from labels
     :param L: 2d label-image
     :param O: 2d orientation-image
     :param n_avg: last n pixels for averaging
-    :return: df["label", "end", "y", "x", "d"]
-        index: (label,end), looks redundant but is convenient
-        end: -1 or 1
-        y, x: average coord of the last n_avg points
-            reducing the probability that ends "nearly overlap"
-        d: average outward direction of the last n_avg points,
-            d(1)-(d(-1)+pi) gives continuous change of directions
+    :return: df_segs, traces
+        df_segs:
+            index=(label,end), looks redundant but is convenient
+            columns=["label", "end", "y", "x", "d"]
+            end: 1/-1, start/end of yx_trace
+            y, x: average coord of the last n_avg points
+                reducing the probability that ends "nearly overlap"
+            d: average outward direction of the last n_avg points,
+                d(-1)-(d(1)+pi) gives continuous change of directions
+        traces: { yx: {label: yx_trace}, d: {label: d_trace} }
     """
     labels = np.unique(L[L > 0])
-    columns = ["label", "end", "y", "x", "d"]
-    data = []
+    df_columns = ["label", "end", "y", "x", "d"]
+    df_data = []
+    traces = {"yx": {}, "d": {}}
     # loop over segments
     for l in labels:
         # trace segment
@@ -174,26 +184,103 @@ def segs_to_frame(L, O, n_avg):
         yx_trace, d_trace, success = trace_within_seg(Ll, O)
         # process non-cyclic segments
         if success:
-            # end -1
+            # record trace
+            traces["yx"][l] = yx_trace
+            traces["d"][l] = d_trace
+
+            # end 1
             data_i1 = [
-                l, -1,  # label, end
+                l, 1,  # label, end
                 *np.mean(yx_trace[:n_avg], axis=0),  # y, x
                 np.mean(d_trace[:n_avg])-np.pi,  # d1, -pi to point outwards
             ]
-            data.append(data_i1)
-            # end 1
+            df_data.append(data_i1)
+            # end -1
             data_i2 = [
-                l, 1,  # label, end
+                l, -1,  # label, end
                 *np.mean(yx_trace[-n_avg:], axis=0),  # y, x
                 np.mean(d_trace[-n_avg:])  # d
             ]
-            data.append(data_i2)
+            df_data.append(data_i2)
     
     # make dataframe, correct dtype, set index
-    df = pd.DataFrame(data=data, columns=columns)
-    df = df.astype({f: int for f in ["label", "end", "y", "x"]})
-    df = df.set_index(zip(df["label"], df["end"]))
-    return df
+    df_segs = pd.DataFrame(data=df_data, columns=df_columns)
+    df_segs = df_segs.astype({f: int for f in ["label", "end", "y", "x"]})
+    df_segs = df_segs.set_index(zip(df_segs["label"], df_segs["end"]))
+    return df_segs, traces
+
+
+#=========================
+# graph tools
+#=========================
+
+def pathL_to_LE(path_l, G):
+    """ convert path of labels to path of (label,end_in)
+    :param path_l: [label1,label2,...]
+    :param G: graph, provides end_in info for each node(label)
+    :return: path_le=[(label1, end_in1),...]
+    """
+    path_le = [(i, G.nodes[i]["end_in"]) for i in path_l]
+    return path_le
+
+def pathLE_to_yx(path, traces_yx, endsegs=(False, False), n_last=1, fill_inter=False):
+    """ convert path of (label,end_in) to yx
+    :param path: [(label1, end_in1),...], at least two elements
+    :param traces_yx: {label1: yx_trace1,...}
+    :param endsegs: if include whole start/end segments
+    :param n_last: start/end if not included in full, use n_last points
+    :param fill_inter: if fills inter-segment gaps by line
+    :return: yx=[(y1,x1),...]
+    """
+    if len(path) < 2:
+        raise ValueError("len(path) should >= 2")
+
+    yx = []
+    # starting segment
+    # end: can be used as step
+    label_s, end_s = path[0]
+    yx_s = traces_yx[label_s][::end_s]
+    yx_s = yx_s if endsegs[0] else yx_s[-n_last:]
+    yx.extend(yx_s)
+
+    # middle segments
+    for i in range(1, len(path)-1):
+        label_i, end_i = path[i]
+        yx_i = traces_yx[label_i][::end_i]
+        # fill inter if selected
+        if fill_inter:
+            inter_yx = list(draw_line(yx[-1], yx_i[0]))
+            yx.extend(inter_yx)
+        yx.extend(yx_i)
+
+    # ending segment
+    label_e, end_e = path[-1]
+    yx_e = traces_yx[label_e][::end_e]
+    yx_e = yx_e if endsegs[1] else yx_e[:n_last]
+    if fill_inter:
+        inter_yx = list(draw_line(yx[-1], yx_e[0]))
+        yx.extend(inter_yx)
+    yx.extend(yx_e)
+
+    yx = np.asarray(yx)
+    return yx
+
+def pathLEs_distance(path1, path2, traces_yx):
+    """ Hausdorff distance between two paths of (label,end_in)
+    :param path1, path2: [(label11, end_in11),...], at least two elements
+    :param traces_yx: {label1: yx_trace1,...}
+    :return: dist(Hausdorff distance)
+    """
+    # convert paths to yxs
+    kwargs = dict(traces_yx=traces_yx, fill_inter=True)
+    yx1 = pathLE_to_yx(path1, **kwargs)
+    yx2 = pathLE_to_yx(path2, **kwargs)
+
+    # compute symmetric Hausdorff distance
+    h12 = sp.spatial.distance.directed_hausdorff(yx1, yx2)[0]
+    h21 = sp.spatial.distance.directed_hausdorff(yx2, yx1)[0]
+    dist = max(h12, h21)
+    return dist
 
 
 #=========================
@@ -229,45 +316,45 @@ def across_next_seg_1end(df_curr, df_next):
     # direction of next wrt curr direction: should < pi/2
     mask["angle"] = np.sum(dvec_curr*dvec_next, axis=1) > 0
     # combine masks
-    mask["combined"] = mask["dir"]&mask["angle"]
+    mask["combined"] = mask["dir"] & mask["angle"]
     return mask
 
-def across_next_seg(label, end, df_segs, dist_cutoff):
+def across_next_seg(label, end, df_segs, search_dist_thresh):
     """ find next segment starting from current one
     :param label, end: segment's label and end
-    :param df_segs: result of segs_to_frame()
-    :param dist_cutoff: discard points >= cutoff
+    :param df_segs: result of trace_within_labels()
+    :param search_dist_thresh: discard points whose distance >= threshold
     :return: df_next[label,end,y,x,d,dist,dd_segs,dd_ends]
         dist: from curr end to next end
         dd_segs: directional change from curr seg to next
         dd_ends: directional change from next seg end to the other end
     """
-    # subset df_segs to curr and next 1 and -1
+    # subset df_segs to curr and next 1(start) and -1(end)
     df_curr = df_segs.loc[(label, end)]
-    df_next1 = df_segs[df_segs["end"] == 1].sort_values("label")
-    df_next2 = df_segs[df_segs["end"] == -1].sort_values("label")
-    assert np.all(df_next1["label"].values == df_next2["label"].values)
+    df_next_s = df_segs[df_segs["end"] == 1].sort_values("label")
+    df_next_e = df_segs[df_segs["end"] == -1].sort_values("label")
+    assert np.all(df_next_e["label"].values == df_next_s["label"].values)
 
     # get masks for two ends of next
-    mask1 = across_next_seg_1end(df_curr, df_next1)
-    mask2 = across_next_seg_1end(df_curr, df_next2)
+    mask_s = across_next_seg_1end(df_curr, df_next_s)
+    mask_e = across_next_seg_1end(df_curr, df_next_e)
     # both ends in plus direction
-    mask_pos = mask1["pos"] & mask2["pos"]
+    mask_pos = mask_s["pos"] & mask_e["pos"]
     # only one end has a small direction change
-    mask_dir = np.logical_xor(mask1["dir"], mask2["dir"])
+    mask_dir = np.logical_xor(mask_s["dir"], mask_e["dir"])
 
     # combine all masks
     # &dir: the end with small dir change is retained
     df_next = pd.concat([
-        df_next1[mask_pos & mask_dir & mask1["dir"] & mask1["angle"]],
-        df_next2[mask_pos & mask_dir & mask2["dir"] & mask2["angle"]]
+        df_next_s[mask_pos & mask_dir & mask_s["dir"] & mask_s["angle"]],
+        df_next_e[mask_pos & mask_dir & mask_e["dir"] & mask_e["angle"]]
     ])
 
     # distance: calculate, thresholding, sort
     dxdy_next = df_next[["x", "y"]].values-df_curr[["x", "y"]].values
     df_next["dist"] = np.linalg.norm(dxdy_next, axis=1)
     df_next = df_next[
-        df_next["dist"] < dist_cutoff
+        df_next["dist"] < search_dist_thresh
     ].sort_values("dist")
 
     # change in directions from current segment to the next
@@ -275,15 +362,97 @@ def across_next_seg(label, end, df_segs, dist_cutoff):
         df_next["d"].values+np.pi-df_curr["d"],
         2*np.pi
     )
-    dd_segs = np.where(dd_segs<=np.pi, dd_segs, dd_segs-2*np.pi)
+    dd_segs = np.where(dd_segs <= np.pi, dd_segs, dd_segs-2*np.pi)
     df_next["dd_segs"] = dd_segs
 
     # change in directions from next segment's end-in to end-out
     n_next = len(df_next)
-    d_end1 = df_segs.loc[zip(df_next["label"], [1]*n_next), "d"].values
-    d_end2 = df_segs.loc[zip(df_next["label"], [-1]*n_next), "d"].values
-    dd_ends = d_end1 - (d_end2 + np.pi)  # d-change from end -1 to 1
-    dd_ends = dd_ends*(-df_next["end"].values)  # consider actual end
+    d_end_s = df_segs.loc[zip(df_next["label"], [1]*n_next), "d"].values
+    d_end_e = df_segs.loc[zip(df_next["label"], [-1]*n_next), "d"].values
+    dd_ends = d_end_e - (d_end_s + np.pi)  # d-change from end -1 to 1
+    dd_ends = dd_ends*df_next["end"].values  # consider actual end
     df_next["dd_ends"] = dd_ends
 
     return df_next
+
+class MemGraph():
+    """ class for building membrane graph recursively
+    __init__: parameters
+        search_dist_thresh: search next segment within this threshold
+        path_dist_thresh: add new edge only if dist >= this threshold
+        dd_thresh: total change in direction should < this threshold
+    build_graph_one: parameters
+        label, end: segment's label and end
+        G: networkx.DiGraph()
+    """
+    def __init__(
+        self, df_segs, traces_yx,
+        search_dist_thresh=50, path_dist_thresh=2, dd_thresh=np.pi/2
+        ):
+        """ init, save parameters """
+        self.df_segs = df_segs
+        self.traces_yx = traces_yx
+        self.search_dist_thresh = search_dist_thresh
+        self.path_dist_thresh = path_dist_thresh
+        self.dd_thresh = dd_thresh
+
+    def build_graph_one(self, label, end, G):
+        """ build graph starting from (label,end)
+        :param label, end: identity of segment end
+        :param G: networkx.DiGraph()
+            node attrs: {end_in, d_in, end_out, d_out}
+                d_in, d_out: wrt root's direction of end_out
+        """
+        # if label not in G, create node
+        if label not in G:
+            G.add_node(
+                label,
+                end_in=-end, d_in=None,
+                end_out=end, d_out=0.
+            )
+
+        # find next segs
+        df_next = across_next_seg(label, end, self.df_segs, self.search_dist_thresh)
+        # update direction 
+        df_next["d"] = G.nodes[label]["d_out"] + df_next["dd_segs"].values
+        df_next["d_out"] = df_next["d"].values + df_next["dd_ends"].values
+
+        for next in df_next.itertuples():
+            # skip if total change in direction >= thresh
+            if ((np.abs(next.d)>=self.dd_thresh)
+                or (np.abs(next.d_out)>=self.dd_thresh)):
+                continue
+
+            # add new node (if not existed)
+            # prepare for visiting the new node
+            if next.label not in G:
+                G.add_node(
+                    next.label,
+                    end_in=next.end, d_in=next.d,
+                    end_out=-next.end, d_out=next.d_out
+                )
+                visit_next = True
+            else:
+                visit_next = False
+
+            # add new edge
+            # (if tentative new edge is not close to existing ones)
+            # can still add edge if node existed
+            add_new_edge = True
+            pathLE_new = pathL_to_LE([label, next.label], G)
+            for pathL_exist in nx.all_simple_paths(G, label, next.label):
+                pathLE_exist = pathL_to_LE(pathL_exist, G)
+                dist_path = pathLEs_distance(
+                    pathLE_new, pathLE_exist, self.traces_yx
+                )
+                # if new is close to one existed, do not add new edge
+                if dist_path < self.path_dist_thresh:
+                    add_new_edge = False
+                    break
+            if add_new_edge:
+                G.add_edge(label, next.label)
+
+            # build graph from new (label,end)
+            if visit_next:
+                self.build_graph_one(next.label, -next.end, G)
+        return
