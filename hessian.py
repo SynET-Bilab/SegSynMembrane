079110df1201f90e1b5860fc8e813613818d26f7
liao_zhenghan <liaozhenghan@outlook.com>
2021-12-28 16:14:44 +0800

    hessian: added surface_norm

--- hessian.py
+++ hessian.py
@@ -4,12 +4,14 @@
 
 import numpy as np
 import numba
-from synseg.utils import gaussian
+from itertools import combinations_with_replacement
+from synseg.utils import gaussian, reverse_coord
 
 __all__ = [
-    "hessian2d",
-    "features2d_H1", "features2d_H2",
-    "features3d"
+    # 2d hessian, saliency
+    "hessian2d", "features2d_H1", "features2d_H2", "features3d",
+    # 3d hessian, surface normal
+    "hessian3d", "symmetric_image", "surface_norm"
 ]
 
 
@@ -18,8 +20,9 @@ __all__ = [
 #=========================
 
 def hessian2d(I, sigma):
-    """ calculate hessian
-    :param sigma: if sigma=0, no gaussian smoothing
+    """ calculate hessian 2d
+    :param I: image, shape=(ny,nx)
+    :param sigma: gaussian smoothing, no smooth if sigma=0
     :return: Hxx, Hxy, Hyy
     """
     # referenced skimage.feature.hessian_matrix
@@ -99,4 +102,91 @@ def features3d(I, sigma):
 
     return S, O
 
+#=========================
+# surface normal
+#=========================
 
+def hessian3d(I, sigma):
+    """ calculate hessian 3d
+    :param I: image, shape=(nz,ny,nx)
+    :param sigma: gaussian smoothing, no smooth if sigma=0
+    :return: Hxx, Hxy, Hxz, Hyy, Hyz, Hzz
+    """
+    # referenced skimage.feature.hessian_matrix
+    # here x,y,z are made explicit
+    # I[z,y,x]
+    Ig = gaussian(I, sigma)
+    Hx = np.gradient(Ig, axis=2)
+    Hy = np.gradient(Ig, axis=1)
+    Hz = np.gradient(Ig, axis=0)
+    Hxx = np.gradient(Hx, axis=2)
+    Hxy = np.gradient(Hx, axis=1)
+    Hxz = np.gradient(Hx, axis=0)
+    Hyy = np.gradient(Hy, axis=1)
+    Hyz = np.gradient(Hy, axis=0)
+    Hzz = np.gradient(Hz, axis=0)
+    return Hxx, Hxy, Hxz, Hyy, Hyz, Hzz
+
+def symmetric_image(S_elems):
+    """ make symmetric image from elements
+    ref: skimage.feature.corner._symmetric_image
+    :param S_elems: e.g. Hxx, Hxy, Hyy
+    :return: H_mats
+        H_mats: H_mats[z,y,x] = hessian matrix at (z,y,x)
+    """
+    image = S_elems[0]
+    symmetric_image = np.zeros(
+        image.shape + (image.ndim, image.ndim),
+        dtype=S_elems[0].dtype
+    )
+    for idx, (row, col) in enumerate(
+        combinations_with_replacement(range(image.ndim), 2)
+    ):
+        symmetric_image[..., row, col] = S_elems[idx]
+        symmetric_image[..., col, row] = S_elems[idx]
+    return symmetric_image
+
+def surface_norm_one(H_mat, del_ref):
+    """ calculate surface normal at one location, via hessian
+    :param H_mat: 3*3 hessian matrix at the location
+    :param del_ref: [x,y,z] of location - [x,y,z] of reference
+    :return: evec_out
+        evec_out: outward normal vector, [x,y,z]
+    """
+    evals, evecs = np.linalg.eigh(H_mat)
+    # max eig by abs
+    imax = np.argmax(np.abs(evals))
+    evec = evecs[:, imax]
+    # align direction with outward
+    evec_out = evec if np.dot(evec, del_ref) >= 0 else -evec
+    return evec_out
+
+@numba.njit(parallel=True)
+def surface_norm(I, xy_ref, sigma=1):
+    """ calculate surface normal at nonzeros of I
+    :param I: image, shape=(nz,ny,nx)
+    :param xy_ref: [x,y] of a reference point inside
+    :param sigma: gaussian smoothing
+    :return: xyz, norm
+        xyz: coordinates, [[x1,y1,z1],...]
+        norm: normal vectors, [[nx1,ny1,nz1],...]
+    """
+    # nonzero pixels
+    pos = np.nonzero(I)
+    n_pts = len(pos[0])
+    # coordinates and hessian
+    with numba.objmode(xyz="int64[:,:]", H_mats="float64[:,:,:]"):
+        xyz = reverse_coord(np.transpose(pos))
+        H_mats = symmetric_image(hessian3d(I, sigma))[pos]
+    # normal
+    norm = np.zeros((n_pts, 3))
+    for i in numba.prange(n_pts):
+        with numba.objmode(norm_i="float64[:]"):
+            norm_i = surface_norm_one(
+                H_mats[i],
+                del_ref=xyz[i]-np.array(
+                    [xy_ref[0], xy_ref[1], xyz[i][-1]]
+                )
+            )
+        norm[i] = norm_i
+    return xyz, norm
