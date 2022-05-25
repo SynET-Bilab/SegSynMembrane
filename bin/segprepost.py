24488bdd6ea2eba0273d069d8bbb27891c704a65
liao_zhenghan <liaozhenghan@outlook.com>
2022-05-25 13:04:07 +0800

    segprepost: mod default params

--- bin/segprepost.py
+++ bin/segprepost.py
@@ -33,17 +33,17 @@ class SegPrePost(etsynseg.segbase.SegBase):
             # detect
             detect_smooth=5,
             detect_tv=20,
-            detect_filt=3.5,
+            detect_filt=4,
             detect_supp=0.5,
             # components
             components_min=0.5,
             # moosac
-            moosac_lengrids=[50, 150],
+            moosac_lengrids=[50, 160],
             moosac_shrinkside=0.25,
             moosac_resize=5,
-            moosac_popsize=40,
+            moosac_popsize=80,
             moosac_tol=0.005,
-            moosac_maxiter=150,
+            moosac_maxiter=200,
         )
 
     def final_results(self):
